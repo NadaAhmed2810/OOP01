8bac2a29fa7596b0c0369a0761b3db43cd0e9387
NadaAhmed2810 <nwrabsywny534@gmail.com>
2024-12-28 00:57:17 +0200

    Exception Handling

--- OOP01/Program.cs
+++ OOP01/Program.cs
@@ -1,10 +1,74 @@
-﻿namespace OOP01
+﻿using System;
+
+namespace OOP01
 {
     internal class Program
     {
+        public static void DoSomeCode()
+        {
+            try {
+                int X = int.Parse(Console.ReadLine()); //FormatException
+                int Y = int.Parse(Console.ReadLine());
+                int Z = X / Y;                         //.DivideByZeroException
+                int[] numbers = { 1, 2, 3 };
+                numbers[10] = 100;          //.IndexOutOfRangeException
+            }
+            catch (Exception ex) { 
+                Console.WriteLine(ex.Message);
+            }
+           
+        }
+        public static void DoSomeprotectiveCode()
+        {
+            try
+            {
+                int X;
+                bool f = false;
+                do
+                {
+                    Console.WriteLine("Enter first Number:");
+                    f = int.TryParse(Console.ReadLine(), out X);
+                } while (!f);
+
+                int Y;
+                do
+                {
+                    Console.WriteLine("Enter Second Number:");
+                    f = int.TryParse(Console.ReadLine(), out Y);
+                } while (!f || Y == 0);
+                int Z = X / Y;
+
+                int[] numbers = { 1, 2, 3 };
+                if (numbers.Length >= 10)
+                    numbers[10] = 100;
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //close ,free,delete,dealocate unmanaged resourse 
+                //open connection Data Base 
+                //open file
+            }
+        
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Exception Handling 
+            DoSomeprotectiveCode();
+            //Exceptions
+            // 1. System Exceptions
+            //// 1.1 Format Exception
+            //// 1.2 Index Out Of Range Exception
+            //// 1.3 Null Reference Exception
+            //// 1.4 Invalid Operation Exception
+            //// 1.5 Arithmetic Exception
+            ////  1.5.1 Divided By Zero Exception
+            ////  1.5.2 Overflow Exception
+            // 2. Application Exception
+
+            #endregion
         }
     }
 }

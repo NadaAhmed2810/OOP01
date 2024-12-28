9a3bc74ac3d20da54159c601a0fe3a2b48843092
NadaAhmed2810 <nwrabsywny534@gmail.com>
2024-12-28 18:33:22 +0200

    Q2

--- Ass/Program.cs
+++ Ass/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Ass
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Ass
 {
     enum WeekDays
     {
@@ -12,6 +14,13 @@
         Sunday
 
     }
+    enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -22,6 +31,34 @@
             //{
             //    Console.WriteLine((WeekDays)i);
             //}
+
+            #endregion
+            #region Q2:Create an enum called "Season" with the four seasons(Spring, Summer, Autumn, Winter) as its members.Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
+            //Season season;
+            //bool f=false;
+            //do
+            //{
+            //    Console.Write("Enter Season:");
+            //    f = Enum.TryParse<Season>(Console.ReadLine(), true, out season);
+            //} while (!f);
+            //if (season == Season.Spring)
+            //{
+            //    Console.WriteLine("Spring:From March to May");
+            //}
+            //else if (season == Season.Summer)
+            //{
+            //    Console.WriteLine("Summer:From June to August");
+            //}
+            //else if (season == Season.Autumn)
+            //{
+            //    Console.WriteLine("Autumn:From September to November");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Winter:From December to February");
+            //}
+
+
 
             #endregion
         }
